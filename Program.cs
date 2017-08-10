c9b8d81c5ebe856397a0279d5c2060051a23ef15
MaxKlaxxMiner <max@tetrisk.de>
2017-08-10 16:13:02 +0200

    initial commit (old project)

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication1
+{
+ public static class Erweiterungen
+ {
+  #region # // --- überladene Werte für einfachere Bedienung ---
+  public static Color ToColor(this int wert)
+  {
+   return Color.FromArgb(wert | -16777216);
+  }
+
+  public static Brush ToBrush(this int wert)
+  {
+   return new SolidBrush(Color.FromArgb(wert | -16777216));
+  }
+
+  public static Pen ToPen(this int wert)
+  {
+   return new Pen(Color.FromArgb(wert | -16777216));
+  }
+  #endregion
+ }
+
+ static class Program
+ {
+  /// <summary>
+  /// Der Haupteinstiegspunkt für die Anwendung.
+  /// </summary>
+  [STAThread]
+  static void Main()
+  {
+   Application.EnableVisualStyles();
+   Application.SetCompatibleTextRenderingDefault(false);
+   Application.Run(new Form1());
+  }
+ }
+}
---
+++ TimerExact.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+ public class TimerExact
+ {
+  Control _parent;
+
+  #region # public static extern void timeBeginPeriod(int tick); // ermöglicht eine Veringerung der Wartephasen zwischen den Windows-Ticks (eine Erhöhung ist nicht möglich und wird ignoriert)
+  /// <summary>
+  /// ermöglicht eine Veringerung der Wartephasen zwischen den Windows-Ticks (eine Erhöhung ist nicht möglich und wird ignoriert)
+  /// </summary>
+  /// <param name="tick">Wartezeit in Millisekunden</param>
+  [DllImport("winmm.dll", SetLastError = true)]
+  public static extern void timeBeginPeriod(int tick);
+  #endregion
+
+  public TimerExact(Control parent)
+  {
+   _parent = parent;
+   timeBeginPeriod(1);
+  }
+
+  public static double TickCount
+  {
+   get
+   {
+    return (double)Stopwatch.GetTimestamp() / (double)Stopwatch.Frequency * 1000.0;
+   }
+  }
+
+  bool aktiv = false;
+
+  Thread laufer = null;
+
+  void Arbeite()
+  {
+   if (_parent != null)
+   {
+    double soll = TickCount;
+    while (aktiv)
+    {
+     try
+     {
+      if (_parent.IsDisposed)
+      {
+       aktiv = false;
+       return;
+      }
+      if (_parent.IsHandleCreated)
+      {
+       _parent.Invoke(new Action(() => Tick(null, EventArgs.Empty)));
+       //Tick(null, EventArgs.Empty);
+      }
+     }
+     catch { }
+     
+     soll += Interval;
+     double ist = TickCount;
+     while (soll > ist)
+     {
+      Thread.Sleep(0);
+      ist = TickCount;
+     }
+
+     if (soll < ist - Interval * 3.0)
+     {
+      soll = ist + Interval * 3.0;
+     }
+
+    }
+   }
+  }
+
+  [DefaultValue(false)]
+  public bool Enabled
+  {
+   get
+   {
+    return aktiv;
+   }
+   set
+   {
+    if (value == aktiv) return;
+    aktiv = value;
+    if (value)
+    {
+     laufer = new Thread(() => { Arbeite(); });
+     laufer.Start();
+    }
+   }
+  }
+
+  [DefaultValue(16.666)]
+  public double Interval { get; set; }
+
+  public event EventHandler Tick;
+
+  ~TimerExact()
+  {
+   aktiv = false;
+  }
+
+  public void Start()
+  {
+   Enabled = true;
+  }
+
+  public void Stop()
+  {
+   Enabled = false;
+  }
+
+  public override string ToString()
+  {
+   return "Timer.";
+  }
+
+
+ }
+
+}
